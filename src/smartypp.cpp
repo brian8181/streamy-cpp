6bd4d66af53436fd2101936a61cbccef2082cfaa
Brian Preston <brian8181@gmail.com>
2022-02-08 12:51:27 -0600

    add class smartpp

---
+++ src/smartypp.cpp
@@ -0,0 +1,21 @@
+#include "smartypp.h"
+
+smartypp::smartypp()
+{
+
+}
+
+smartypp::smartypp(string template_dir, string complie_dir, string config_dir, string cache_dir) 
+{
+
+}
+
+smartypp::~smartypp()
+{
+
+}
+
+bool smartypp::assign(string name, string val)
+{
+    return true;
+}
---
+++ src/smartypp.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <map>
+
+//using std::string;
+//using std::map;
+
+using namespace std;
+
+class smartypp
+{
+public:
+    smartypp();
+    smartypp(std::string template_dir, std::string complie_dir, std::string config_dir, std::string cache_dir);
+    ~smartypp();
+    bool assign(std::string name, std::string val);
+
+private:
+
+    map<std::string, std::string> symbols;
+
+    std::string template_dir;
+    std::string compile_dir;
+    std::string config_dir;
+    std::string cache_dir;
+};
