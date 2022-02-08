30757d94b7bf231c0e465618f94b8dd3abc290bc
Brian Preston <brian8181@gmail.com>
2022-02-08 12:23:42 -0600

    consolidate dumps

--- cgi/dump_get.cpp
+++ cgi/dump_get.cpp
@@ -15,11 +15,11 @@ int main(int argc, char* argv[])
         string src0 = ifstream_readlines(path);
         cout << src0 << endl;
 
-        cout << endl << endl << "fstream_read" << endl;
-        string src1 = fstream_read(path);
-        cout << src1 << endl;
+        // cout << endl << endl << "fstream_read" << endl;
+        // string src1 = fstream_read(path);
+        // cout << src1 << endl;
 
-        cout << endl << endl << "fstream_read" << endl;
+        cout << endl << "fstream_read" << endl;
         string src2 = ifstream_readlines(path);
         cout << src2 << endl;
     }
