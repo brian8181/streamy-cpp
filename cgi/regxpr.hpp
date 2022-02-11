49b709981db97cf23441e34f6c3f06e2755b659d
Brian Preston <brian8181@gmail.com>
2022-02-10 19:42:01 -0600

    add regx stuff

--- cgi/regxpr.hpp
+++ cgi/regxpr.hpp
@@ -5,6 +5,10 @@
 
 using std::string;
 
+
+string s1 = R"([-!"#$%&'()*+,./:;<=>?@[\\\]^_`{|}~])";
+string punct = R"([-!"#%&'()*,./:;?@[\\\]_{}])"; //ascii
+string sybols = R"([$+<=>^`|~])"; //ascii
 string name_exp = "[A-Za-z]+\\w*";
 string var = "\\{\\s*\\$(" + name_exp + ")\\s*\\}";
 
