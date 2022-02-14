bd6d321585e0961ad73b6236e7726a7e4832be44
Brian Preston <brian8181@gmail.com>
2022-02-14 10:32:19 -0600

    add to to smarty class

--- src/smarty.cpp
+++ src/smarty.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
+#include <regex>
+#include <fstream>
 #include "smarty.h"
 
-smarty::smarty(string template_dir, string compile_dir, string config_dir, string cache_dir) 
+smarty::smarty(const string& template_dir, const string& complie_dir, const string& config_dir, const string& cache_dir)
 {
     this->template_dir = template_dir;
     this->compile_dir = compile_dir;
@@ -13,14 +16,101 @@ smarty::~smarty()
 
 }
 
-bool smarty::assign(string name, string val)
+bool smarty::assign(const string& name, const string& val)
 {
     pair<string, string> p(name, val);
     symbols.insert(p);
     return true;
 }
 
-bool smarty::display(string tmpl)
+bool smarty::display(const string& tmpl)
 {
+    string src = readlines(tmpl);
+    regex exp = regex("\\{\\$(.*?)\\}", regex::ECMAScript);
+            
+    sregex_iterator begin = sregex_iterator(src.begin(), src.end(), exp);
+    auto end = sregex_iterator(); 
+    
+    int beg_pos = 0;
+    string output;
+    for (sregex_iterator iter = begin; iter != end; ++iter)
+    {
+        smatch match = *iter;
+        std::ssub_match sub = match[1];
+        string tag = trim(sub.str());
+        
+        int end_pos = match.position();
+        output += src.substr(beg_pos, end_pos-beg_pos);
+        map<string, string>::const_iterator find_iter = symbols.find(tag);
+        if(find_iter != symbols.end())
+        {
+            output += find_iter->second;
+        }
+        beg_pos = end_pos + match.length();
+    }
+    output += src.substr(beg_pos);
+    cout << output << endl;
+
     return true;
 }
+
+string smarty::readlines(const string& path)
+{
+    string src;
+    fstream file;
+    file.open(path, ios::in); //open a file to perform read operation using file object
+    if (file.is_open())
+    {   
+        //checking whether the file is open
+        string tp;
+        while(getline(file, tp))
+        { 
+            //read data from file object and put it into string.
+            src += tp += "\n";
+        }
+        file.close(); //close the file object.
+    }
+    return src;
+}
+
+std::string smarty::ltrim(const std::string &s)
+{
+    size_t start = s.find_first_not_of(WHITESPACE);
+    return (start == std::string::npos) ? "" : s.substr(start);
+}
+
+std::string smarty::rtrim(const std::string &s)
+{
+    size_t end = s.find_last_not_of(WHITESPACE);
+    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
+ 
+std::string smarty::trim(const std::string &s)
+{
+    return rtrim(ltrim(s));
+}
+
+// std::string& ltrim(std::string &s)
+// {
+//     auto it = std::find_if(s.begin(), s.end(),
+//                     [](char c) {
+//                         return !std::isspace<char>(c, std::locale::classic());
+//                     });
+//     s.erase(s.begin(), it);
+//     return s;
+// }
+ 
+// std::string& rtrim(std::string &s)
+// {
+//     auto it = std::find_if(s.rbegin(), s.rend(),
+//                     [](char c) {
+//                         return !std::isspace<char>(c, std::locale::classic());
+//                     });
+//     s.erase(it.base(), s.end());
+//     return s;
+// }
+ 
+// std::string& trim(std::string &s)
+// {
+//     return ltrim(rtrim(s));
+// }
--- src/smarty.h
+++ src/smarty.h
@@ -6,16 +6,27 @@
 
 using namespace std;
 
+const string WHITESPACE = " \n\r\t\f\v";
+
 class smarty
 {
 public:
-    smarty(string template_dir, string complie_dir, string config_dir, string cache_dir);
+    smarty(const string& template_dir, const string& complie_dir, const string& config_dir, const string& cache_dir);
     ~smarty();
 
-    bool assign(string name, string val);
-    bool display(string tmpl);
+    bool assign(const string& name, const string& val);
+    bool display(const string& tmpl);
 
 private:
+
+    string readlines(const string& path);
+    string ltrim(const string &s);
+    string rtrim(const string &s);
+    string trim(const string &s);
+
+    // std::string& ltrim(const std::string &s);
+    // std::string& rtrim(const std::string &s);
+    // std::string& trim(const std::string &s);
 
     map<string, string> config;
     map<string, string> symbols;
