847dd5025dd5788869c089116f6c650eeba98b91
Brian K Preston <brian8181@gmail.com>
2022-07-21 07:32:34 -0500

    add expressions

--- src/regexpr.hpp
+++ src/regexpr.hpp
@@ -6,12 +6,21 @@
 
 using namespace std;
 
+// https://cplusplus.com/reference/regex/ECMAScript/
+
+string s1 = R"([-!"#$%&'()*+,./:;<=>?@[\\\]^_`{|}~])";
+string punct = R"([-!"#%&'()*,./:;?@[\\\]_{}])"; //ascii
+string symbols = R"([$+<=>^`|~])"; //ascii
+string name_exp = "[A-Za-z]+\\w*";
+string var = "\\{\\s*\\$(" + name_exp + ")\\s*\\}";
+string command = "(config_load|include|insert)\\sfile='(.*)'";
+
 const string COMMAND = "(config_load|include|insert|literal|strip|capture|section|assign|debug|eval|fetch|math)";
 const string ESCAPE = "\\{\\$?(.*?)\\}";
 const string ESCAPE2 = "\\{\\(.*)\\}";
 const string COMMENT = "\\n?\\{\\s*\\*[\\w\\s\\p]*\\*\\s*\\}\\n?";
 const string CONFIG = "\\{\\s*#([A-Aa-z]\\w*)#\\s*\\}";
-const string SYMBOL_NAME = "[_.~]*[A-Za-z][A-Za-z0-9_.-~]*";
+const string SYMBOL_NAME = "\\b[_.~]*[A-Za-z][\\w_.-~]*\\b";
 const string VARIABLE = "\\{\\s*\\$(" + SYMBOL_NAME + ")\\s*\\}";
 const string VARIABLE2 = "\\$(" + SYMBOL_NAME + ")";
 // todo default values ...
@@ -29,7 +38,10 @@ const string string_literal1 = "(\\\"[\\w\\.]*\\\")"; // "ABC"
 const string string_literal2 = "(\\'[\\w*\\.]*\\')";  // 'ABC'
 const string string_literal = string_literal1 + "|" + string_literal2;
 
-const string IF_STMT = "\\{if\\s+\\(.*?)\\}";
-const string ARRAY_DECL = "array\\(" + string_literal1 + "|" + string_literal1 + "\\)";
+const string IF_KEYWORD = "\\{if\\(" + VARIABLE + ")\\}";
+const string ENDIF_KEYWORD = "\\{/if\\s+}";
+const string HTML = IF_KEYWORD + "([\\w<=>]*)" + ENDIF_KEYWORD;
+const string ELSE_KEYWORD = "\\{if\\s+\\(.*?)\\}";
+const string ARRAY_KEYWORD = "array\\(" + string_literal1 + "|" + string_literal1 + "\\)";
 
 #endif
