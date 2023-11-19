434d58b3d1fec3f326563db30367343bb26537d7
Brian K <brian8181@gmail.com>
2023-11-19 09:45:28 -0600

    rearrage streamy.hpp ...

--- src/streamy.hpp
+++ src/streamy.hpp
@@ -7,9 +7,9 @@
 
 using namespace std;
 
+const string ESCAPE = "\\{\\$?(.*?)\\}";
 const string SYMBOL_NAME = "\\b[_.~]*[A-Za-z][A-Za-z0-9_.-~]*\\b";
 const string LOAD_CONFIG_PAIR = "([A-Za-z][A-Za-z0-9]*)=([A-Za-z0-9]*);";
-const string ESCAPE = "\\{\\$?(.*?)\\}";
 const string INCLUDE = "\\{\\s*\\include file\\s*=\\s*\"(.*?)\"\\s*\\}";
 const string VARIABLE = "\\{\\s*\\$(" + SYMBOL_NAME + ")\\s*\\}";
 const string COMMENT = "\\n?\\{\\s*\\*[\\w\\s\\p]*\\*\\s*\\}\\n?";
@@ -23,38 +23,34 @@ public:
     bool assign(const string& name, const string& val);
     template <class T> bool assign(const string& name, vector<T>& values);
     bool display(const string& tmpl);
-
-public:
-    string& lex(const string& tmpl, /*out*/ string& s_out);
-
-private:
-    string read_stream(const string& path);
-    string include(const string& tmpl);
-    string variable(const string& src);
-    string remove_comments(const string& tmpl);
-    string if_sequence(const string& src);
-    string replace_tag(string& tmpl, const string& exp_str);
-    string& trim(string &s, char c);
-    string& ltrim(std::string &s);
-    string& rtrim(std::string &s);
-    string& trim(std::string &s);
-    // test functons
-    string get_conf(string s);
-
-public:
-    // maps
+     // maps
     std::map<string, string> streamy_vars;
     std::map<string, string> config;
     std::map<string, string> vars;
     std::map<string, vector<string>> var_arrays;
     std::map<string, std::map<string, string>> arrays;
-
+   
 private:
+    string read_stream(const string& path);
+    string& trim(string &s, char c);
+    string& ltrim(std::string &s);
+    string& rtrim(std::string &s);
+    string& trim(std::string &s);
     // config
     string template_dir;
     string compile_dir;
     string config_dir;
     string cache_dir;
+
+public:
+    string& lex(const string& tmpl, /*out*/ string& s_out);
+    string include(const string& tmpl);
+    string variable(const string& src);
+    string remove_comments(const string& tmpl);
+    string if_sequence(const string& src);
+    string replace_tag(string& tmpl, const string& exp_str);
+     // test functons
+    string get_conf(string s);
     // todo INACTIVE
     string left_delimiter = "{";
     string right_delimiter = "}";
