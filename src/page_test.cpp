672b9c1ea2ce3e1f8cfdf7464403c762c97f1b1f
Brian K <brian8181@gmail.com>
2023-12-21 16:03:33 -0600

    add page_test.cgi

---
+++ src/page_test.cpp
@@ -0,0 +1,107 @@
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// License:    GPL
+// Author:     Brian K Preston
+// File Name:  ./index.cpp
+// Build Date: Thu Dec 21 09:06:55 AM CST 2023
+// Version:    0.0.2
+
+#include <iostream>
+#include <cstring>
+#include <unistd.h>         /* for STDIN_FILENO */
+#include <sys/select.h>     /* for pselect   */
+#include <string>
+#include <getopt.h>
+#include "streamy.hpp"
+
+using namespace std;
+
+int parse_options(int argc, char* argv[])
+{
+    // testing values ...  
+    vector<string> citys = { "Mesquite",  "Dallas", "Addison", "New York",     "London", 
+                             "Barcelona", "Madrid", "Paris",   "Las Angelels", "Las Vegas", 
+                             "Garland",   "Richardson", "Plano"};
+
+    // initial configuration ...
+    const string project_folder = "/home/brian/src/streamy-cpp";
+    const string config_path = "./test/conf";
+
+    // out variables : so the function may return by reference not value
+    std::map<string, string> config;
+    std::map<string, string> vars;
+    string s_out;
+
+    // steamy-cpp ...
+	streamy sm(project_folder + "/test/templates", project_folder + "/test/compile", project_folder + "/test/config", project_folder + "/test/cache");
+    sm.load_config(config_path, s_out);
+    sm.assign("citys", citys);
+    sm.get_map_config(config).insert(pair<string, string>("version", "2000"));
+    sm.get_map_config(config).insert(pair<string, string>("mail_message", "No Mail!"));
+    sm.get_map_config(config).insert(pair<string, string>("admin_email", "webmaster@streamy-cpp.com"));
+    sm.get_map_vars(vars).insert(pair<string, string>("version", "2000"));
+    sm.get_map_vars(vars).insert(pair<string, string>("mail_message", "No Mail!"));
+    sm.get_map_vars(vars).insert(pair<string, string>("admin_email", "webmaster@streamy-cpp.com"));
+    sm.assign("headers", "HEADERS");
+    sm.assign("page_title", "*PAGE_TITLE*");
+    sm.assign("body", "**THE BODY**");
+    sm.assign("admin_email", "admin@something.com");
+    sm.assign("version", "0.1");
+    sm.assign("version_date", "Feb, 14 2022");
+    sm.display("test_vars.tpl");
+
+    // read / display ...
+    map<string, string>::iterator end = sm.get_map_config(config).end();
+    for (map<string, string>::iterator iter = sm.get_map_config(config).begin(); iter != end; ++iter)
+    {
+        cout << "key: " << iter->first << " , value: " << iter->second << endl;
+    }
+    return 0;
+}
+
+int stdin_ready (int filedes)
+{
+	fd_set set;
+	// declare/initialize zero timeout ...
+	struct timespec timeout = { .tv_sec = 0 };
+	// initialize the file descriptor set ...
+	FD_ZERO(&set);
+	FD_SET(filedes, &set);
+	// check stdin_ready is ready on filedes ...
+	return pselect(filedes + 1, &set, NULL, NULL, &timeout, NULL);
+}
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		if(stdin_ready(STDIN_FILENO))
+		{
+			std::string buffer;
+			std::cin >> buffer;
+			// add piped buffer to end of argv ...
+			char* argvtmp[sizeof(char*) * argc+1];
+			memcpy(argvtmp, argv, sizeof(char*) * argc);
+			argvtmp[argc] = &buffer[0];
+			argv = argvtmp;
+			++argc;
+		}
+		return parse_options(argc, argv);
+	}
+	catch(std::logic_error& ex)
+	{
+	 	std::cout << ex.what() << std::endl;
+	}
+}
+
