0303487bc0eaceee2b6ddbe5cf59c94c8d3154d0
Brian K <brian8181@gmail.com>
2024-01-04 19:53:29 -0600

    work in token ids, for parsing / grammar rules ...

--- src/tokens.hpp
+++ src/tokens.hpp
@@ -47,20 +47,23 @@ const string KEY_WORDS = "(if)|(else)|(elseif)|(foreach)|(foreachelse)|(literal)
 
 enum en_token
 {
-    ID_OPEN_CURLY_BRACE = 0x400,
-    ID_CLOSE_CURLY_BRACE = 0x800,
-    ID_IF = 0x01,
-    ID__ELSE = 0x02,
-    ID_ELSEIF = 0x04,
-    ID_FOREACH = 0x80,
-    ID_FOREACHELSE = 0x100,
-    ID_LITERAL     = 0x200,
-    ID_ASTERIK = 0x1000,
-    ID_HASH_MARK = 0x2000,
-    ID_DOLLAR_SIGN = 0x4000,
-    ID_DOT = 0x8000,
-    ID_EQUAL = 0x10000,
-    ID_FILE_FUNCTION = 0x20000
+    ID_IF                 = 0x01,
+    ID_ELSE               = 0x02,
+    ID_ELSEIF             = 0x04,
+    ID_FOREACH            = 0x08,
+    ID_FOREACHELSE        = 0x0100,
+    ID_LITERAL            = 0x0200,
+    ID_OPEN_CURLY_BRACE   = 0x0400,
+    ID_CLOSE_CURLY_BRACE  = 0x0800,
+    ID_ASTERIK            = 0x1000,
+    ID_HASH_MARK          = 0x2000,
+    ID_DOLLAR_SIGN        = 0x4000,
+    ID_DOT                = 0x8000,
+    ID_EQUAL              = 0x10000,
+    ID_FILE_FUNCTION      = 0x20000,
+    ID_VBAR               = 0x40000,
+    ID_COLON              = 0x80000,
+    ID_SEMI_COLON         = 0x100000
 };
 
 const string REGEXP_TOKENS = "(" + VARIABLE_OFFSETS + ")|(" + ACTION_TOKEN + ")|(" + KEY_WORDS + ")";
